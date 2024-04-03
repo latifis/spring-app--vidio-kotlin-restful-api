355d3623870ef7316dd69799bb09e17180dc1d71
latif <latifirfansyah@gmail.com>
2024-04-03 08:53:08 +0700

    initial

---
+++ src/main/kotlin/com/latif/vidio/VidioApplication.kt
@@ -0,0 +1,11 @@
+package com.latif.vidio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VidioApplication
+
+fun main(args: Array<String>) {
+	runApplication<VidioApplication>(*args)
+}
