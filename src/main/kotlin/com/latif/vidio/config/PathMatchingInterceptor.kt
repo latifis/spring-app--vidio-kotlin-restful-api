d16baa63015bdb9f843d6e8ac1d5468ea0ff659d
latif <latifirfansyah@gmail.com>
2024-04-04 03:59:21 +0700

    add interceptor

---
+++ src/main/kotlin/com/latif/vidio/config/PathMatchingInterceptor.kt
@@ -0,0 +1,22 @@
+package com.latif.vidio.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class PathMatchingInterceptor (
+    val authInterceptor: AuthInterceptor,
+    val requestInterceptor: RequestInterceptor
+) : WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(requestInterceptor)
+
+        registry.addInterceptor(authInterceptor).excludePathPatterns(
+//            "/v1/api/login"
+            "/v1/api/auth/login"
+        )
+    }
+
+}
